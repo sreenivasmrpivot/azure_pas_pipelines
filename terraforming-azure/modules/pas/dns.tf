a6e8e497f158119e23e1ab689aae1919271491f9
Michael Lipscombe <mlipscombe@outlook.com>
2019-07-12 11:41:07 -0600

    corrected ip name

--- terraforming-azure/modules/pas/dns.tf
+++ terraforming-azure/modules/pas/dns.tf
@@ -19,7 +19,7 @@ resource "azurerm_dns_a_record" "iso" {
   zone_name           = "${var.dns_zone_name}"
   resource_group_name = "${var.resource_group_name}"
   ttl                 = "60"
-  records             = ["${azurerm_public_ip.pks-lb-public-ip.ip_address}"]
+  records             = ["${azurerm_public_ip.pks-lb-ip.ip_address}"]
 }
 
 resource "azurerm_dns_a_record" "sys" {
